f68c313ddc63f2b208b8b84133dd649fa8e5dc98
EntropicalEmily <dicearrows@gmail.com>
2026-02-01 10:49:47 +0100

    Replaced Player Sprites and made the toggle visual.

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,13 +7,22 @@ var movement_input: Vector2
 var mouse_position: Vector2
 var atack_on_cooldown: bool = false
 
+var is_in_cyberspace: bool = false
+
+@onready var gun_sprite_rs: Sprite2D = $GunRealspace/Sprite2D
+@onready var gun_sprite_cs: Sprite2D = $GunCyberspace/Sprite2D
+@onready var player_sprite_realspace: AnimatedSprite2D = $PlayerSpriteRealspace
+@onready var player_sprite_cyberspace: AnimatedSprite2D = $PlayerSpriteCyberspace
+@onready var cyber_space_layer: Sprite2D = $"../CyberSpaceLayer"
+
+
+
 @export var bullet_scene: PackedScene
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 func _process(_delta: float) -> void:
-	movement_animation()
 	mouse_position = get_global_mouse_position()
 	#look_at(mouse_position)
 	#use this to turn the player in the direction of the mouse
@@ -29,6 +38,8 @@ func _process(_delta: float) -> void:
 		player_health -= 20
 	if Input.is_action_just_pressed("damage_player_firewall_debug"):
 		player_firewall -= 20
+	if Input.is_action_just_pressed("cybermask_toggle"):
+		toggle_player_appearance()
 
 func _physics_process(_delta: float) -> void:
 	movement_input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -41,14 +52,18 @@ func shoot():
 	bullet.global_position = PlayerData.gun_position
 	bullet.aim((PlayerData.crosshair_location - global_position).normalized())
 
-func movement_animation():
-	if movement_input.x > 0:
-		$Sprite2D.play("move_right")
-	elif movement_input.x < 0:
-		$Sprite2D.play("move_left")
-	elif movement_input.y < 0:
-		$Sprite2D.play("move_up")
-	elif movement_input.y > 0:
-		$Sprite2D.play("move_down")
+func toggle_player_appearance():
+	if !is_in_cyberspace:
+		cyber_space_layer.visible = true
+		is_in_cyberspace = true
+		player_sprite_realspace.visible = false
+		player_sprite_cyberspace.visible = true
+		gun_sprite_rs.visible = false
+		gun_sprite_cs.visible = true
 	else:
-		$Sprite2D.play("idle")
+		is_in_cyberspace = false
+		cyber_space_layer.visible = false
+		player_sprite_realspace.visible = true
+		player_sprite_cyberspace.visible = false
+		gun_sprite_rs.visible = true
+		gun_sprite_cs.visible = false
