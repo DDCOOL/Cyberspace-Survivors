a8409ffe6477a5bcb0fb57184f3757adf93deeb8
CoLouRiSTiCx <152689821+CoLouRiSTiCX@users.noreply.github.com>
2026-02-01 13:11:45 +0100

    added main menu scene transitions

--- scenes/ui/main_menu_control.gd
+++ scenes/ui/main_menu_control.gd
@@ -2,6 +2,7 @@ extends Control
 
 signal startGame
 
+var button_type = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,7 +15,9 @@ func _process(delta: float) -> void:
 
 
 func _on_start_game_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/Level_1.tscn")
+	button_type = "start"
+	$scene_transition/Timer.start()
+	$scene_transition/AnimationPlayer.play("easse_out")
 
 
 func _on_settings_button_pressed() -> void:
@@ -23,9 +26,19 @@ func _on_settings_button_pressed() -> void:
 
 
 func _on_exit_button_pressed() -> void:
-	get_tree().quit()
+	button_type = "quit"
+	$scene_transition/Timer.start()
+	$scene_transition/AnimationPlayer.play("easse_out")
 
 
 func _on_settings_menu_is_closed() -> void:
 	$"../SettingsMenu".hide()
 	$"../MainMenuControl".show()
+
+
+func _on_fade_timer_timeout() -> void:
+	if button_type == "start":
+		get_tree().change_scene_to_file("res://scenes/levels/Level_1.tscn")
+	
+	if button_type == "quit":
+		get_tree().quit()
