fd93f1b9b4310634684f2acf75bcd2b83e75cb6d
LateNightChosis <chosisradio@gmail.com>
2026-02-01 15:15:56 +0100

    basic pathfinding für knife-gegner eingefügt (still broken)

--- scripts/realspace_knifer_lvl_1.gd
+++ scripts/realspace_knifer_lvl_1.gd
@@ -1,14 +1,30 @@
 extends CharacterBody2D
 
+
 var enemy_health: int = 10
 var speed: int = 120
 var direction: Vector2
 
+var navPath
+var currPathPoint 
+
+var path_point_margin: float = 0.5
+
+func _ready() -> void:
+	setTarget()
+
+func setTarget() -> void:
+	navPath = NavigationServer2D.map_get_path(get_world_2d().get_navigation_map(), global_position, PlayerData.player_location, true)
+	currPathPoint = 0
+
 func _physics_process(_delta: float) -> void:
-	if PlayerData.player_location:
-		#look_at(PlayerData.player_location)
-		direction =  PlayerData.player_location - position
-	velocity = (direction).normalized() * speed
+	if global_transform.origin.distance_to(navPath[currPathPoint]) <= path_point_margin:
+		currPathPoint += 1
+		if currPathPoint >= navPath.size():
+			currPathPoint -= 1
+	#look_at(PlayerData.player_location)
+	direction =  global_transform.origin.direction_to(navPath[currPathPoint])
+	velocity = direction.normalized() * speed
 	move_and_slide()
 
 
@@ -28,3 +44,7 @@ func take_damage():
 func _on_hurtbox_body_entered(body: Node2D) -> void:
 	take_damage()
 	body.queue_free()
+
+
+func _on_timer_timeout() -> void:
+	setTarget()
